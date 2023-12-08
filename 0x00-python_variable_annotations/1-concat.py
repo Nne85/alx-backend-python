a7d803b6e287b077d8ee425cbed123b2a88246f4
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-12-08 21:36:30 +0000

    Concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" This module concatennates two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function uses type-annotation to concatente two strings
+    """
+    return str1 + str2
