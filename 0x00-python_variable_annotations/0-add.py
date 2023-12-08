45b5cbe2836f3960a47b069ebe087387d0119c5d
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-12-08 21:20:49 +0000

    Add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,7 +2,7 @@
 """ This module displaye a type-annotated function"""
 
 
-def add(a: float, b: float):
+def add(a: float, b: float) -> float:
     """
     This function add that takes a float a and a float b as arguments
     and returns their sum as a float.
