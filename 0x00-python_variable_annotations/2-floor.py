7e1080aa121d38463de57387d0335fea809d06bd
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-12-08 21:46:51 +0000

    Math Floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" This module returns the floor of a float """
+import math
+
+
+def floor(n: float) -> float:
+    """ This function floor which takes a float n as argument and
+    returns the floor of the float.
+    """
+    return math.floor(n)
