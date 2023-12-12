770ad14b58cb26ce03835434c2e7b0c4b16b2804
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-12-12 14:42:13 +0000

    Async Comprehension Tasks

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" This module has function called async_generator that takes no arguments"""
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ This function called async_generator that takes no arguments."""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This module handles Async Comprehensions"""
+
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """This function return the 10 random numbers.
+
+    Args:
+        None
+
+    Return:
+        return the 10 random numbers.
+    """
+    return [i async for i in async_generator()]
